fb5e999dd66a4097cf3ecbf2ae91f8e76010185c
Pra-06 <150579100+Pra-06@users.noreply.github.com>
2025-03-14 14:53:14 +0530

    adding current files

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,25 @@
-console.log('first')
-console.log("first")
-console.log("first")
-console.log("first")
+import express from "express"
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000
+dotenv.config()
+app.use(express.json())
+
+/* app.get("/",(req,res)=> {
+    //root route http://localhost:5000/
+    res.send("Hello World!");
+}) */;
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`)
+
+}
+
+    );
